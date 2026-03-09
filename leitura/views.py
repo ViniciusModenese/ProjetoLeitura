59d2f89b56f6aae7263836c1c16e4712d2f6a6cb
Vinicius Modenese <modenesevini@gmail.com>
2026-03-09 20:41:54 -0300

    setup inicial do django com sqlite3 e models de leitura concluídos

--- leitura/views.py
+++ leitura/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Livro
 
-# Create your views here.
+def home(request):
+    # Busca todos os livros cadastrados no banco de dados
+    livros = Livro.objects.all()
+    # Envia essa lista para o arquivo HTML (Template)
+    return render(request, 'leitura/home.html', {'livros': livros})
