419dc2d7ce5fe5c2d9ba8f037389b8c71524f24a
Vinicius Modenese <modenesevini@gmail.com>
2026-03-14 12:34:37 -0300

    adicionando home.html

--- leitura/views.py
+++ leitura/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from .models import Livro
 
 def home(request):
-    # Busca todos os livros cadastrados no banco de dados
     livros = Livro.objects.all()
-    # Envia essa lista para o arquivo HTML (Template)
     return render(request, 'leitura/home.html', {'livros': livros})
--- literamatch_project/urls.py
+++ literamatch_project/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.status),
+    path('admin/', admin.site.urls), # O erro estava aqui! Agora está corrigido.
     path('', include('leitura.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
